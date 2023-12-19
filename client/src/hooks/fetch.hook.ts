8dbfcc6a172fce38fd2b07dfd135fcc0db2c1401
Frosty <nayanbastola777@gmail.com>
2023-12-18 23:12:52 -0500

    Hooks Init

---
+++ client/src/hooks/fetch.hook.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+/** SERVER DOMAIN - BASE URL */
+axios.defaults.baseURL = import.meta.env.REACT_APP_SERVER_URL;
+
+/** CUSTOM HOOKS */
+export default function useFetch(query) {
+  const [getData, setData] = useState({
+    isLoading: false,
+    apiData: undefined,
+    status: null,
+    serverError: null,
+  });
+
+  useEffect(() => {
+    if (!query) return;
+
+    const fetchData = async () => {
+      try {
+        setData((prev) => ({ ...prev, isLoading: true }));
+
+        const { data, status } = await axios.get(`/api/${query}`);
+        if (status === 201) {
+          setData((prev) => ({ ...prev, isLoading: false }));
+          setData((prev) => ({ ...prev, apiData: data, status: status }));
+        }
+        setData((prev) => ({ ...prev, isLoading: false }));
+      } catch (error) {
+        setData((prev) => ({ ...prev, isLoading: false, serverError: error }));
+      }
+    };
+    fetchData();
+  }, [query]);
+  return [getData, setData];
+}
